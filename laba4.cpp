ffe0d29a9a46c4eed4fe8319635fd829e7229de7
Void-Frog1 <146322102+Void-Frog1@users.noreply.github.com>
2023-11-09 14:11:26 +0300

    laba4.cpp

---
+++ laba4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0,"");
+    int arr[6]{
+        1 ,-2 ,56 ,-34 ,23 ,-1
+    };
+    for (int i = 0; i < 6; i++)
+    {
+        if (arr[i] >= 0)
+            cout << arr[i];
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        if (arr[i] < 0)
+            cout << arr[i];
+    }
+ }
